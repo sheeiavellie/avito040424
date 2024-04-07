c7d23e20a69786d115381f7679681f9f31611dc2
sheeiavellie <josephlandmars@gmail.com>
2024-04-07 19:34:00 +0300

    feat: add roles

---
+++ api/roles.go
@@ -0,0 +1,18 @@
+package api
+
+var (
+	AdminRole = accessRole{name: "admin"}
+	UserRole  = accessRole{name: "user"}
+)
+
+type AccessRole interface {
+	GetName() string
+}
+
+type accessRole struct {
+	name string
+}
+
+func (ar *accessRole) GetName() string {
+	return ar.name
+}
