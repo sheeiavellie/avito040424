13344a90eff639416d74ff6b8ce9d271fd95b180
sheeiavellie <josephlandmars@gmail.com>
2024-04-11 00:51:04 +0300

    fix: update interface

--- storage/storage.go
+++ storage/storage.go
@@ -1,7 +1,16 @@
 package storage
 
+import (
+	"context"
+	"hash"
+
+	"github.com/sheeiavellie/avito040424/data"
+)
+
 type Storage interface {
 }
 
 type CacheStorage interface {
+	GetBanner(ctx context.Context, bannerKey hash.Hash32) (*data.Banner, error)
+	SetBanner(ctx context.Context, banner *data.Banner) error
 }
