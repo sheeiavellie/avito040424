fb2c1585037dedd5dc904404ac69fe3727643bbd
sheeiavellie <josephlandmars@gmail.com>
2024-04-11 02:16:24 +0300

    feat: add close and execTx methods

--- storage/postgres.go
+++ storage/postgres.go
@@ -3,6 +3,7 @@ package storage
 import (
 	"context"
 	"database/sql"
+	"fmt"
 
 	_ "github.com/lib/pq"
 	"github.com/sheeiavellie/avito040424/data"
@@ -14,6 +15,38 @@ type PostgresStorage struct {
 
 func NewPostgresStorage() *PostgresStorage {
 	return &PostgresStorage{}
+}
+
+func (ps *PostgresStorage) Close() error {
+	err := ps.db.Close()
+	return fmt.Errorf("error closing db: %w", err)
+}
+
+func (ps *PostgresStorage) execTx(
+	ctx context.Context,
+	opts *sql.TxOptions,
+	query string,
+	queryArgs ...any,
+) error {
+	tx, err := ps.db.BeginTx(ctx, opts)
+	if err != nil {
+		return fmt.Errorf("error executing tx: %w", err)
+	}
+	defer func() {
+		_ = tx.Rollback()
+	}()
+
+	_, execErr := tx.ExecContext(ctx, query, queryArgs...)
+	if execErr != nil {
+		txErr := tx.Rollback()
+		return fmt.Errorf("error executing tx: %w, %w", execErr, txErr)
+	}
+
+	if err := tx.Commit(); err != nil {
+		return fmt.Errorf("error executing tx: %w", err)
+	}
+
+	return nil
 }
 
 func (ps *PostgresStorage) GetBanners(
