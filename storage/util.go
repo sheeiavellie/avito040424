1fe6e3189eeb3dca971aec9e2c31b60edfc61fd1
sheeiavellie <josephlandmars@gmail.com>
2024-04-14 00:59:47 +0300

    feat: add new error types

--- storage/util.go
+++ storage/util.go
@@ -3,5 +3,9 @@ package storage
 import "fmt"
 
 var (
-	ErrorBannerIsNotActive = fmt.Errorf("banner is not active")
+	ErrorBannerIsNotActive     = fmt.Errorf("banner is not active")
+	ErrorFeatureOrTagDontExist = fmt.Errorf("feature or tag doesn't exist")
+	ErrorBannerAlreadyExist    = fmt.Errorf(
+		"banner with given feature and tags already exists",
+	)
 )
