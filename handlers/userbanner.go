2a5958be662897875581159f97385ce96784f20f
sheeiavellie <josephlandmars@gmail.com>
2024-04-14 23:35:44 +0300

    feat: add context with timeout to handler

--- handlers/userbanner.go
+++ handlers/userbanner.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/schema"
 	"github.com/sheeiavellie/avito040424/data"
@@ -15,44 +16,64 @@ import (
 
 func HandleGetUserBanner(
 	ctx context.Context,
+	timeout time.Duration,
 	bannerRepo repository.BannerRepository,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if err := r.ParseForm(); err != nil {
-			log.Printf("an error occur at HandleGetUserBanner: %s", err)
-			util.SetHTTPErrorInternalServerError(w)
-			return
-		}
+		ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+		defer cancel()
 
-		var bannerReq data.UserBannerRequest
-		if err := schema.NewDecoder().Decode(&bannerReq, r.Form); err != nil {
-			log.Printf("an error occur at HandleGetUserBanner: %s", err)
-			util.SetHTTPErrorBadRequest(w)
-			return
-		}
+		done := make(chan struct{})
 
-		banner, err := bannerRepo.GetBannerContent(
-			ctx,
-			bannerReq.FeatureID,
-			bannerReq.TagIDs,
-			bannerReq.UseLastRevision,
-		)
-		if err != nil {
-			log.Printf("an error occur at HandleGetUserBanner: %s", err)
-			switch {
-			case errors.Is(err, storage.ErrorBannerIsNotActive):
-				util.SetHTTPErrorConflict(w)
-			default:
+		go func() {
+			defer func() {
+				done <- struct{}{}
+			}()
+			if err := r.ParseForm(); err != nil {
+				log.Printf("an error occur at HandleGetUserBanner: %s", err)
 				util.SetHTTPErrorInternalServerError(w)
+				return
 			}
-			return
-		}
 
-		err = util.WriteJSON(w, http.StatusOK, banner)
-		if err != nil {
-			log.Printf("an error occur at HandleGetUserBanner: %s", err)
-			util.SetHTTPErrorInternalServerError(w)
-			return
+			var bannerReq data.UserBannerRequest
+			if err := schema.NewDecoder().Decode(&bannerReq, r.Form); err != nil {
+				log.Printf("an error occur at HandleGetUserBanner: %s", err)
+				util.SetHTTPErrorBadRequest(w)
+				return
+			}
+
+			banner, err := bannerRepo.GetBannerContent(
+				ctx,
+				bannerReq.FeatureID,
+				bannerReq.TagIDs,
+				bannerReq.UseLastRevision,
+			)
+			if err != nil {
+				log.Printf("an error occur at HandleGetUserBanner: %s", err)
+				switch {
+				case errors.Is(err, storage.ErrorBannerDontExist):
+					util.SetHTTPErrorNotFound(w)
+				case errors.Is(err, storage.ErrorBannerIsNotActive):
+					util.SetHTTPErrorConflict(w)
+				default:
+					util.SetHTTPErrorInternalServerError(w)
+				}
+				return
+			}
+
+			err = util.WriteJSON(w, http.StatusOK, banner)
+			if err != nil {
+				log.Printf("an error occur at HandleGetUserBanner: %s", err)
+				util.SetHTTPErrorInternalServerError(w)
+				return
+			}
+		}()
+
+		select {
+		case <-ctx.Done():
+			util.SetHTTPErrorRequestTimeout(w)
+		case <-done:
+
 		}
 	}
 }
